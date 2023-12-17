fe152d70509f0e3487d94674e4a79217f80e382b
commonPlaceC <aalex.03@mail.ru>
2023-12-17 16:32:06 +0300

    initial commit

---
+++ server/db.js
@@ -0,0 +1,3 @@
+const { MongoCient } = require("mongodb");
+
+const URL = "localhost:27017/HotelDb";
--- server/server.js
+++ server/server.js
@@ -1,26 +1,7 @@
 const express = require("express");
 
-const app = express();
-
-const cors = require("cors");
-
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-
-app.use(express.json());
-
-app.use(require("./routes/record"));
-
-// Get MongoDB driver connection
-const dbo = require("./db/conn");
+const PORT = 5000;
  
-app.listen(port, () => {
-  // Perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`Listening port ${PORT}`);
 });
